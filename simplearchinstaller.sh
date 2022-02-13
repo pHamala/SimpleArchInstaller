474f964df91fc7e4ead0d244341f3585dc0dacab
pHamala <petrihamalainen@outlook.com>
2022-02-13 14:42:43 +0200

    Update simplearchinstaller.sh

--- simplearchinstaller.sh
+++ simplearchinstaller.sh
@@ -25,6 +25,11 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 '
 
+# Log script
+exec 3>&1 4>&2
+trap 'exec 2>&4 1>&3' 0 1 2 3
+exec 1>log.out 2>&1
+
 clear
 simplearchinstaller (){
 echo -ne "
